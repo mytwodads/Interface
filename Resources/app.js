6d5fe36d8eed1ed39b7e531ea9972514d5f54124
Alex <alex@milkus.com>
2010-10-07 18:48:54 -0400

    second commit

--- Resources/app.js
+++ Resources/app.js
@@ -50,6 +50,11 @@ var messageText = Titanium.UI.createTextArea({
 	height:200,
 	top:40,
 	editable:true,
+	appearance:Titanium.UI.KEYBOARD_APPEARANCE_ALERT,
+});
+
+messageText.addEventListener('blur',function(){
+	secretButton.show();
 });
 
 contentOverlay.add(messageText);
@@ -58,27 +63,103 @@ var secretButton = Titanium.UI.createButton({
 	title:"Enter a secret word",
 	font:{fontSize:20,fontFamily:"Comic Zine OT"},
 	color:"#aa224c",
-	backgroundColor:"fffff4",
-	top:280,
-	left:40,
+	backgroundColor:"none",
+	top:300,
+	left:20,
 	height:40,
 	width:220,
 	borderColor:"none",
 	borderWidth:0,
 	borderRadius:0,
-	borderColor:"#fffff4",
-	style:Titanium.UI.iPhone.SystemButtonStyle.PLAIN
+	style:Titanium.UI.iPhone.SystemButtonStyle.PLAIN,
+	appearance:Titanium.UI.KEYBOARD_APPEARANCE_ALERT,
 });
 
 contentOverlay.add(secretButton);
+secretButton.hide();
+
+secretButton.addEventListener('click',function(){
+
+	if (encodeSwitch){
+		encodeSwitch.hide();
+		secretButton.hide();
+	}
+
+	middleView.animate({top:0,duration:250});
+	bottomView.animate({top:0,duration:250}, function(){
+
+	var w = Ti.UI.createWindow({
+		backgroundImage:'paper2.png'
+	});
+	
+	var secretWord = Titanium.UI.createTextField({
+		autocorrect: false,
+		height:32,
+		hintText:"Enter a secret word",
+		width:160,
+		font:{fontSize:13},
+		color:'#333',
+		paddingLeft:10,
+		top:200,
+		borderStyle:Titanium.UI.INPUT_BORDERSTYLE_NONE,
+		appearance:Titanium.UI.KEYBOARD_APPEARANCE_ALERT,
+	});
+	
+	secretWord.addEventListener('blur',function(){
+		if (secretWord.value.length < 2) {
+			secretWord.focus();
+			alert("Please enter at least 2 characters");
+			Titanium.Media.vibrate();
+		}
+		else {
+		secretButton.title = "";
+		var timer = setTimeout(function(){
+			middleView.animate({top:30, duration:250}); //animations can take an optional callback function which makes stringing functions together super easy
+			bottomView.animate({top:30, duration:250}, function(){
+				encodeSwitch.show();
+				secretButton.show();
+				secretButton.title = secretWord.value;
+			});	
+		},500);
+		w.close();
+		}
+		});
+	
+	w.add(secretWord);
+	w.open({modal:true,modalTransitionStyle:Ti.UI.iPhone.MODAL_TRANSITION_STYLE_PARTIAL_CURL,modalStyle:Ti.UI.iPhone.MODAL_PRESENTATION_PAGESHEET,navBarHidden:true});
+	});
+});
 
 var encodeSwitch = Titanium.UI.createSwitch({
-	top: 200,
+	top: 305,
 	right: 40,
 	value:false,
 });
 
 contentOverlay.add(encodeSwitch);
+encodeSwitch.hide();
+
+encodeSwitch.addEventListener('change',function(){
+	if (encodeSwitch.value) {
+		bottomView.animate({top:80, duration:250}, function(){
+			sendButton.animate({opacity:1,duration:150});
+			resetButton.animate({opacity:1,duration:150});
+		});
+		messageText.editable = false;
+		messageText.color = "#666";
+		secretButton.enabled = false;
+		secretButton.color = "#666";
+	}
+	else {
+		bottomView.animate({top:-30, duration:250});
+		sendButton.animate({opacity:0,duration:150});
+		resetButton.animate({opacity:0,duration:150});
+		messageText.editable = true;
+		messageText.color = "#aa224c";
+		secretButton.enabled = true;
+		secretButton.color = "#aa224c";
+	}
+});
 
 var sendButton = Titanium.UI.createButton({
 	title:"Tweet",
@@ -97,6 +178,7 @@ var sendButton = Titanium.UI.createButton({
 });
 
 contentOverlay.add(sendButton);
+sendButton.opacity = 0;
 
 var resetButton = Titanium.UI.createButton({
 	title:"Reset",
@@ -115,6 +197,22 @@ var resetButton = Titanium.UI.createButton({
 });
 
 contentOverlay.add(resetButton);
+resetButton.opacity = 0;
+
+resetButton.addEventListener('click',function(){
+	secretButton.hide();
+	encodeSwitch.hide();
+	encodeSwitch.value = false;
+	sendButton.opacity = 0;
+	resetButton.opacity = 0;
+	secretButton.title = "Enter a secret word";
+	secretButton.enabled = true;
+	secretButton.color = "#aa224c";
+	messageText.editable = true;
+	messageText.color = "#aa224c";
+	middleView.animate({top:0, duration:250});
+	bottomView.animate({top:0, duration:250});
+	});
 
 
 baseWindow.add(topView);
